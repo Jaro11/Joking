7bf2aed11ee9f016182a7dfd2aa0aacaf9ecf679
Jaroslav Sidor <jaroslav.sidor.ml@gmail.com>
2024-05-09 14:22:24 +0200

    added JS

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,10 +1,12 @@
-class JokesController < ApplicationController
-  require 'net/http'
-  require 'uri'
-  require 'json'
+require 'net/http'
 
+class JokesController < ApplicationController
   def show
     @joke = fetch_joke
+    respond_to do |format|
+      format.html # continues to respond to HTML requests as before
+      format.json { render json: { joke: @joke } } # respond to JSON requests
+    end
   end
 
   private
@@ -12,6 +14,16 @@ class JokesController < ApplicationController
   def fetch_joke
     uri = URI.parse("https://v2.jokeapi.dev/joke/Any")
     response = Net::HTTP.get_response(uri)
-    return JSON.parse(response.body)["joke"] if response.is_a?(Net::HTTPSuccess)
+    if response.is_a?(Net::HTTPSuccess)
+      JSON.parse(response.body)["joke"]
+    else
+      puts "Failed to fetch joke: #{response.code} #{response.message}"
+      nil
+    end
+  rescue JSON::ParserError => e
+    puts "JSON Parsing Error: #{e.message}"
+  rescue => e
+    puts "General Error: #{e.message}"
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'jokes#show'
+  get 'jokes/show', to: 'jokes#show'
 end
